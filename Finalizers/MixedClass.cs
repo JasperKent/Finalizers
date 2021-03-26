a6b072a2b1551b34207f04bbe538719b9e8daec2
Jasper Kent <jasper_kent@hotmail.com>
2021-03-26 08:19:28 +0000

    Add project files.

---
+++ Finalizers/MixedClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Finalizers
+{
+    public class MixedClass : IDisposable
+    {
+        private StreamWriter _writer;
+        private Excel.Application _excel;
+
+        private bool disposedValue;
+
+        public void StartWriting()
+        {
+            _writer = new StreamWriter("output.txt");
+            _excel = new Excel.Application();
+        }
+
+        [SupportedOSPlatform("windows")]
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _writer?.Dispose();
+                    Console.WriteLine("Disposing of writer");
+                }
+
+                if(_excel != null)
+                {
+                    _excel.Quit();
+                    Marshal.ReleaseComObject(_excel);
+                    Console.WriteLine("Releasing Excel");
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        [SupportedOSPlatform("windows")]
+        ~MixedClass()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        [SupportedOSPlatform("windows")]
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Finalizers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Finalizers
+{
+    class Program
+    {
+        static void Run()
+        {
+            using MixedClass mc = new MixedClass();
+
+            mc.StartWriting();
+        }
+
+        static void Main()
+        {
+            Run();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
